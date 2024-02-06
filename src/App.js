a3470c753a36c6396a3502fee7da2f18ce17ccd2
thokar10 <sabin1076@gmail.com>
2024-02-06 16:57:56 +0545

    Message

--- src/App.js
+++ src/App.js
@@ -1,20 +1,103 @@
 import "./app.css";
+import "./font.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faCompassDrafting } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
+
 function App() {
   return (
     <>
-      <div className="header">
-        <div className="namePerson">Sujan Pokhrel</div>
-      </div>
-
-      <div className="middle">
-        <div className="image"></div>
-        <div className="name-person" style={{ color: "black" }}>
-          <p style={{ display: "inline", textDecoration: "underline" }}>
-            Full Stacker Developer
-          </p>
-          &nbsp;(MERN STACK)
+      {/* nav-bar */}
+      <div className="blue-colour">
+        <div className="header">
+          <div className="name">
+            <p>Sabin Thokar</p>
+          </div>
+          <div className="link">
+            <a className="mylink" href="">
+              Contact
+            </a>
+            <a href="">Media</a>
+            <a href="">Projects</a>
+            <a href="">Resume</a>
+            <a href="">Resources</a>
+            <div className="sv-image">
+              <FontAwesomeIcon icon={faEnvelope} />
+              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faLinkedinIn} />
+            </div>
+          </div>
         </div>
-        <div className="mail">mail me : Sujan901@gmail.com </div>
+
+        {/* body */}
+        <div className="bdy">
+          <div className="circle-image"></div>
+          <div className="information">
+            <h3>
+              I am full stack developer and i m very passionate about my work.
+              With over 10 years of experience in full stack development,i can
+              create quality apps and websites for your company.If you want me
+              to build a website ,please contact me and we will get in touch
+              soon.
+            </h3>
+          </div>
+        </div>
+
+        <div class="center">
+          <div class="cdiv"></div>
+        </div>
+
+        <div class="center">
+          <p class="what">What I DO !</p>
+        </div>
+
+        <div className="skills">
+          <div className="box">
+            <div>
+              <FontAwesomeIcon className="icon" icon={faCompassDrafting} />
+            </div>
+            <h3>Web development</h3>
+            <p>
+              I can develop responsive websites and deliver you at the quickest
+              time. Trust me on that one!
+            </p>
+          </div>
+          <div className="box">
+            <div>
+              <FontAwesomeIcon className="icon" icon={faPenNib} />
+            </div>
+            <h3>Web development</h3>
+            <p>
+              I can develop responsive websites and deliver you at the quickest
+              time. Trust me on that one!
+            </p>
+          </div>
+        </div>
+
+        <div className="contact">
+          <div className="contact-image"></div>
+          <div className="details">
+            <h1>Contact me!</h1>
+            your name:
+            <div className="msg-box"></div>
+            mobile number:
+            <div className="msg-box"></div>
+            message:
+            <div className="msg-box1"></div>
+          </div>
+        </div>
+
+        <div className="footer">
+          <div className="location">
+            <p>
+              Bouddha,Nayabasti, sabin1076@gmail.com &copy; All rights Reserved
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
