e56eb8c49b7f60cc37f632509619d83388700135
thokar10 <sabin1076@gmail.com>
2024-02-07 15:07:07 +0545

    firs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./app.css";
 import "./font.css";
+import luffy from "./svg images/1.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faPenNib } from "@fortawesome/free-solid-svg-icons";
@@ -26,16 +27,19 @@ function App() {
             <a href="">Resume</a>
             <a href="">Resources</a>
             <div className="sv-image">
-              <FontAwesomeIcon icon={faEnvelope} />
-              <FontAwesomeIcon icon={faTwitter} />
-              <FontAwesomeIcon icon={faLinkedinIn} />
+              <FontAwesomeIcon className="top" icon={faEnvelope} />
+              <FontAwesomeIcon className="top" icon={faTwitter} />
+              <FontAwesomeIcon className="top" icon={faLinkedinIn} />
             </div>
           </div>
         </div>
 
         {/* body */}
         <div className="bdy">
-          <div className="circle-image"></div>
+          <div
+            className="circle-image"
+            style={{ backgroundImage: `url(${luffy})` }}
+          ></div>
           <div className="information">
             <h3>
               I am full stack developer and i m very passionate about my work.
@@ -57,9 +61,7 @@ function App() {
 
         <div className="skills">
           <div className="box">
-            <div>
-              <FontAwesomeIcon className="icon" icon={faCompassDrafting} />
-            </div>
+            <FontAwesomeIcon className="icon" icon={faCompassDrafting} />
             <h3>Web development</h3>
             <p>
               I can develop responsive websites and deliver you at the quickest
@@ -67,10 +69,9 @@ function App() {
             </p>
           </div>
           <div className="box">
-            <div>
-              <FontAwesomeIcon className="icon" icon={faPenNib} />
-            </div>
-            <h3>Web development</h3>
+            <FontAwesomeIcon className="icon" icon={faPenNib} />
+
+            <h3>UI / UX</h3>
             <p>
               I can develop responsive websites and deliver you at the quickest
               time. Trust me on that one!
@@ -78,24 +79,32 @@ function App() {
           </div>
         </div>
 
-        <div className="contact">
-          <div className="contact-image"></div>
-          <div className="details">
-            <h1>Contact me!</h1>
-            your name:
-            <div className="msg-box"></div>
-            mobile number:
-            <div className="msg-box"></div>
-            message:
-            <div className="msg-box1"></div>
+        <div className="project">
+          <div className="project-idea">
+            <h1>Have any project on mind?</h1>
           </div>
         </div>
+        <form>
+          <div className="contact">
+            <div className="contact-image"></div>
+            <div className="details">
+              <h1>Contact me!</h1>
+              your name:
+              <input type="text" className="msg-box" />
+              mobile number:
+              <input type="text" className="msg-box" />
+              message:
+              <input type="text" className="msg-box1" />
+            </div>
+          </div>
+        </form>
 
-        <div className="footer">
-          <div className="location">
-            <p>
-              Bouddha,Nayabasti, sabin1076@gmail.com &copy; All rights Reserved
-            </p>
+        <div class="footer">
+          <div class="text">
+            <div class="address">
+              <div>Bouddha Nayabasti,</div>
+              <div>sabin1076@gmail.com.np.&copy; All right reserved.</div>
+            </div>
           </div>
         </div>
       </div>
